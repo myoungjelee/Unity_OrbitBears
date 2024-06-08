fdce8cd7056dd75e7b3478e8e8f988203777af85
Hyerimhaha <appled418@gmail.com>
2024-06-08 17:08:05 +0900

    Reapply "데이터 생성 완료, 충돌 시 처리 구현"
    This reverts commit a4acfb7bbd56b2709ca0d27633d48e977440c140.

--- Assets/Scripts/PlanetManager.cs
+++ Assets/Scripts/PlanetManager.cs
@@ -1,6 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-<<<<<<< HEAD
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEditor.Networking.PlayerConnection;
@@ -33,21 +32,4 @@ public class PlanetManager : ScriptableObject
         planetComponent.radius = planetData.radius;
         planetComponent.nextSizeSprite = planetData.nextSizeSprite;
     }   
-=======
-using UnityEngine;
-
-public class PlanetManager : MonoBehaviour
-{   
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
->>>>>>> dev
 }
