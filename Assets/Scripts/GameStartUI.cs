1f57fab7f926fce30fc3a687cbdb4f47132c6158
myoungjelee <120433909+myoungjelee@users.noreply.github.com>
2024-06-10 22:22:28 +0900

    시작 씬 머지

--- Assets/Scripts/GameStartUI.cs
+++ Assets/Scripts/GameStartUI.cs
@@ -5,27 +5,10 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameStartUI : MonoBehaviour
-{
-    private TextMeshProUGUI playButton;
-    public GameObject PlayUI;
-
-    public void OnEnable()
-    {
-        // "Score Text (TMP)" 이름을 가진 게임 오브젝트를 찾아 TextMeshProUGUI 컴포넌트를 할당
-        playButton = GameObject.Find("Play Button").GetComponent<TextMeshProUGUI>();
-        
-    }
-
+{ 
     public void PlayGame()
     {
         // 씬을 로드하여 게임을 시작합니다.
-        SceneManager.LoadScene("Test_MJ");
+        SceneManager.LoadScene("Solar_System");
     }
-
-
-    public void Play()
-    {
-        PlayUI.SetActive(true);
-    }
-
 }
